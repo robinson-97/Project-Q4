088dad57410168d7e7bad55dcffe3f7f259775c4
Kevin van Hal <kevinvhal2005@gmail.com>
2023-05-31 10:13:26 +0200

    menu function

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php echo get_header( ) ?>
+<?php echo wp_nav_menu( array( 'theme_location' => 'extra-menu' ) ); ?>
 <h1> <?php echo get_the_title( ) ?></h1>
 <p> <?php echo get_the_content()?></p>
-<p> hello world</p>
+
 <div id="box">
 </div>
