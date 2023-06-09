12e32393948c875d7a2834603cad96ecdb89414c
robinson <robinson.lachaud@gmail.com>
2023-06-09 12:06:00 +0200

    navbar

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<?php echo get_header( ) ?>
-<?php echo wp_nav_menu( array( 'theme_location' => 'extra-menu' ) ); ?>
-<h1> <?php echo get_the_title( ) ?></h1>
-<p> <?php echo get_the_content()?></p>
+<?php echo get_header() ?>
+<?php echo wp_nav_menu(array('theme_location' => 'extra-menu')); ?>
+<h1> <?php echo get_the_title() ?></h1>
+<p> <?php echo get_the_content() ?></p>
 
-<div id="box">
-</div>
+
+<h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem cumque delectus ducimus ea eaque eligendi
+    harum ipsam itaque labore nihil odio praesentium quo recusandae reiciendis repellendus, sint totam
+    voluptatibus!</h1>
+
+<div class="div1"> <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab et iure magni non odit. Amet animi aspernatur, at
+    beatae dignissimos eos hic magni, modi, qui rem repellendus saepe voluptate voluptatum. Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Alias deserunt eligendi modi molestias neque odit optio quod quos ratione veritatis.
+    Aperiam blanditiis consequuntur et facilis harum inventore iusto laboriosam quod! Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Adipisci amet animi architecto, cum eos esse et facilis, hic ipsum iste iusto
+    molestias nam praesentium, quasi quia ratione sapiente sint sunt? Lorem ipsum dolor sit amet, consectetur
+    adipisicing elit. Cum dolor libero possimus quis veniam? Amet autem dolores excepturi exercitationem id laborum,
+    maiores nobis odio perspiciatis, ratione, repellendus repudiandae tempore voluptas.</p></div>
+
+
+
